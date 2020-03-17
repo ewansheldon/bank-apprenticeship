33c8c16d6ed4ec1c8d5254c6abb4c0c39b5fd1a5
Alexis Pavlidis <apavlidi@it.teithe.gr>
2020-03-17 16:46:53 +0000

    red test for cacluating running balance on print statements

--- src/test/java/com/codurance/bank/ui/PrinterImplementationShould.java
+++ src/test/java/com/codurance/bank/ui/PrinterImplementationShould.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.codurance.bank.domain.Transaction;
 import com.codurance.bank.testDoubles.ConsoleSpy;
-import java.util.Calendar;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.GregorianCalendar;
 import org.junit.jupiter.api.Test;
@@ -31,6 +31,20 @@ class PrinterImplementationShould {
         printer.print(Collections.singletonList(transaction));
 
         assertEquals("Date || Amount || Balance\n" +
+            "14/01/2012 || 1000 || 1000", console.verifyPrinted());
+    }
+
+    @Test
+    public void print_transactions_with_balance() {
+        ConsoleSpy console = new ConsoleSpy();
+        PrinterImplementation printer = new PrinterImplementation(console);
+
+        printer.print(Arrays.asList(
+            new Transaction(1000, new GregorianCalendar(2012, JANUARY, 14)),
+            new Transaction(-2000, new GregorianCalendar(2013, JANUARY, 14))));
+
+        assertEquals("Date || Amount || Balance\n" +
+            "14/01/2013 || -2000 || -1000\n" +
             "14/01/2012 || 1000 || 1000", console.verifyPrinted());
     }
 
